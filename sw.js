071cac0ce0f84b64e99384949d2613534cd723fd
natanlimamoura-maker <natan.limamoura@gmail.com>
2026-01-28 18:50:02 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+// Esse script permite que o navegador reconheça o site como um App Instalável
+const CACHE_NAME = 'patio-pro-cache-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['./', './index.html']);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});

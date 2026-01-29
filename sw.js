98b4085e71560c5d0feb5025c8ceda491b15b097
natanlimamoura-maker <natan.limamoura@gmail.com>
2026-01-29 13:13:29 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const cacheName = 'patiopro-v1';
+const CACHE_NAME = 'patio-pro-v3';
 const assets = ['index.html', 'manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', e => {
